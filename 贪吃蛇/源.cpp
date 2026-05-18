981070063256d820142e78042dee684133c075e4
Kingchin <2165149639@qq.com>
2026-05-18 20:48:34 +0800

    更新每一个难度的最高得分

--- 贪吃蛇/源.cpp
+++ 贪吃蛇/源.cpp
@@ -10,6 +10,9 @@
 #define Red "\033[31m"
 #define Green "\033[32m"
 #define White "\033[0m"
+#define Blue "\033[34m"
+#define Yellow "\033[33m"
+
 #define Row 20
 #define Col 20
 
@@ -40,7 +43,9 @@ vector<Pos> snake;//保存每段蛇体的位置
 Pos applePos;//苹果生成的位置
 int direction = Right;//当前蛇的方向
 int mode = 300;//蛇移动的速度
-int max_score = 0;//最高得分！
+int max_score1 = 0;//简单最高得分
+int max_score2 = 0;//中等最高得分
+int max_score3 = 0;//困难最高得分
 char map[Row][Col] =//用char类型读写更快速！
 {
 	{9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9},
@@ -65,29 +70,32 @@ char map[Row][Col] =//用char类型读写更快速！
 	{9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9}
 };
 char frame[Row][Col];//用于打印的缓存
+int overall_select = 0;//用户的选择
 void getScore();
 void menu()
 {
 	getScore();
-	cout << "---------------"<<Green<<"贪吃蛇"<<White<<"--------------- " << endl;
+	cout << Green<<"--------------"<<"贪吃蛇"<<"-------------- " <<White<< endl;
 	cout << "欢迎来到贪吃蛇游戏  本游戏由"<<Red<<"柒"<<White<<"开发" << endl<<endl;
 	cout << "游戏规则:" << endl;
 	cout << "	 W A S D 控制移动" << endl;
 	cout << "	 吃果子变长" << endl;
 	cout << "	 碰到墙游戏结束" << endl;
 	cout << "	 吃到苹果即可得分" << endl<<endl;
-	cout << "历史最高得分:" << max_score << endl;
-	cout << "-----------享受你的游戏吧------------" << endl;
+	cout <<Yellow<< "-----------"<<"历史最高一览"<<"------------"<<White << endl<<endl;
+	cout <<Green<< "简单难度" << White"历史最高得分: " << max_score1 << endl;
+	cout <<Blue<<"中等难度" << White"历史最高得分: " << max_score2 << endl;
+	cout <<Red<< "困难难度" << White"历史最高得分: " << max_score3 << endl<<endl;
+	cout << "-------------开始游戏--------------" << endl;
 	cout << "选择难度" << endl;
 	cout << "按下数字并回车以选择" << endl;
-	cout << "1.简单" << endl;
-	cout << "2.中等" << endl;
-	cout << "3.困难" << endl;
-	int select = 0;
+	cout << "1.Easy - 简单" << endl;
+	cout << "2.Medium-中等" << endl;
+	cout << "3.Hard - 困难" << endl;
 	while (1)
 	{
-		cin >> select;
-		switch (select)
+		cin >> overall_select;
+		switch (overall_select)
 		{
 		case 1:
 			mode = 200;
@@ -108,6 +116,7 @@ void menu()
 tag:;
 	system("pause");
 }
+
 void test01()
 {
 	menu();
@@ -115,7 +124,9 @@ void test01()
 void putScore()
 {
 	ofstream ofs("score.txt");
-	ofs << max_score;
+	ofs << max_score1 << endl;
+	ofs << max_score2 << endl;
+	ofs << max_score3 << endl;
 	ofs.close();
 }
 void getScore()
@@ -123,7 +134,7 @@ void getScore()
 	ifstream ifs("score.txt");
 	if (ifs)
 	{
-		ifs >> max_score;
+		ifs >> max_score1>>max_score2>>max_score3;
 	}
 	ifs.close();
 
@@ -257,14 +268,28 @@ int main()
 			//cout.flush();
 			cout << endl << "游戏结束" << endl;
 			score = snake.size() - 3;
-			if (score > max_score)
-			{
-				max_score = score;
-				putScore();
-				getScore();
-			}
 			cout << "本次得分：" << score << endl;
-			cout << "历史最高：" << max_score << endl;
+			if (overall_select == 1 && score > max_score1)
+			{
+				max_score1 = score;
+				putScore();			
+			}
+			if (overall_select == 2 && score > max_score2)
+			{
+				max_score2 = score;
+				putScore();
+			}
+			if (overall_select == 3 && score > max_score3)
+			{
+				max_score3 = score;
+				putScore();
+			}
+			if (overall_select == 1)
+				cout << "历史最高" << max_score1 << endl;
+			if (overall_select == 2)
+				cout << "历史最高" << max_score2 << endl;
+			if (overall_select == 3)
+				cout << "历史最高" << max_score3 << endl;
 			break;
 		}
 	}
